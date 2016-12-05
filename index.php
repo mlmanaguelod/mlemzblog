7922ed7e686111a4221cb72c2f7a8a1f943f939b
ML Managuelod <mlmanaguelod@gmail.com>
2016-12-05 12:26:12 +0800

    blah

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>MLemz Blog</title>
+	<link href="https://fonts.googleapis.com/css?family=Englebert" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="main.css">
+</head>
+<body>
+
+</body>
+</html>
